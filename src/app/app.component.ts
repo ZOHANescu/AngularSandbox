84c5483a4f64003b4fa75d4d61727f7626c36338
StefanH <stefanhera@gmail.com>
2023-03-15 07:36:19 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css'
+  ]
 })
+
 export class AppComponent {
-  title = 'basics';
+
+    name = "Mircea"
+    imageUrl = "https://picsum.photos/id/237/500/500";
+
+    getName () {
+        return this.name;
+    }
+
+    changeImage(e: KeyboardEvent) {
+        this.imageUrl = (e.target as HTMLInputElement).value;
+    }
 }
