6f7a728478be20773b9ed2b7924fba4d834ac49e
StefanH <stefanhera@gmail.com>
2023-03-15 14:10:20 +0200

    Understanding pipes

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,11 +1,17 @@
-import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
+import { 
+    Component, Input, EventEmitter, Output, OnInit, OnChanges, DoCheck, SimpleChanges,
+    AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy
+} from '@angular/core';
 
 @Component({
     selector: 'app-post',
     templateUrl: './post.component.html',
     styleUrls: ['./post.component.css']
 })
-export class PostComponent implements OnInit {
+export class PostComponent implements 
+    OnInit, OnChanges, DoCheck, 
+    AfterContentInit, AfterContentChecked, 
+    AfterViewInit, AfterViewChecked, OnDestroy {
 
     @Input('img')
     postImage = "";
@@ -14,10 +20,38 @@ export class PostComponent implements OnInit {
     imageSelected = new EventEmitter<string>()
 
     constructor() {
-        console.log("constructor() called.", this.postImage);
+        console.log("constructor() called", this.postImage);
     }
 
     ngOnInit() {
-        console.log('ngOnInit() called.', this.postImage);
+        console.log('ngOnInit() called', this.postImage);
+    }
+
+    ngDoCheck() {
+        console.log("ngDoCheck() called");
+    }
+    
+    ngOnChanges() {
+        console.log("ngOnChanges() called");
+    }
+
+    ngAfterContentInit() {
+        console.log("ngAfterContentInit() called");
+    }
+
+    ngAfterContentChecked() {
+        console.log("ngAfterContentChecked() called");
+    }
+
+    ngAfterViewInit() {
+        console.log("ngAfterViewInit() called");
+    }
+
+    ngAfterViewChecked() {
+        console.log("ngAfterViewChecked() called");
+    }
+
+    ngOnDestroy() {
+        console.log("onDestroy() called");
     }
 }
